78806c5c255debf39ed63a49093c02aab07e3e6f
Andrew Blakely <andrew.blakely@ymail.com>
2015-07-30 02:12:37 -0500

    Travis Issues
    Fixed assembly name of Tests suite.
    
    Fixed namespaces of Tests project.

--- LoggingServices.Tests/Logging Tests/AsyncTextWriterLoggerTest.cs
+++ LoggingServices.Tests/Logging Tests/AsyncTextWriterLoggerTest.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace LoggingServices.Test
+namespace LoggingServices.Tests
 {
 	[TestFixture]
 	public static class AsyncTextWriterLoggerTest
--- LoggingServices.Tests/Logging Tests/LogMessageTests.cs
+++ LoggingServices.Tests/Logging Tests/LogMessageTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LoggingServices.Test
+namespace LoggingServices.Tests
 {
 	[TestFixture]
 	public static class LogMessageTests
