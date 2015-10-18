91236c671210642f0a1d87261d77188f11738b4e
Andrew Blakely <andrew.blakely@ymail.com>
2015-10-18 02:31:28 -0500

    Enabled CLSCompliant

--- src/LoggingServices/Properties/AssemblyInfo.cs
+++ src/LoggingServices/Properties/AssemblyInfo.cs
@@ -1,10 +1,12 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: CLSCompliantAttribute(true)]
 [assembly: AssemblyTitle("LogginServices")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
